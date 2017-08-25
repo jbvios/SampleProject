015714c32b19958c63c520f430bcb7bb7fdeb3d8
John Bryan Vios <viosjb@gmail.com>
2017-08-25 12:53:35 +0800

    Edit Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Framework
                 </div>
 
                 <div class="links">
